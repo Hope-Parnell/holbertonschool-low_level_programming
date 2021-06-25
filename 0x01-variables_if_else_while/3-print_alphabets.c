aea2c5b7f11d3f33b79b6feec900dec5a657cd9f
Hope-Parnell <parnell.hope.c@gmail.com>
2021-06-25 16:37:43 +0000

    update function description comment

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - prints the lowercase alphabet one letter per line
+ * main - prints the lowercase alphabet followed by the uppercase alphabet
  *
  * Return: 0
  */
