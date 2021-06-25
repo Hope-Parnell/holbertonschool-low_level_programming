fcd60840a1437ddf3fee82f57365446ae885c0ae
Hope-Parnell <parnell.hope.c@gmail.com>
2021-06-25 16:18:53 +0000

    add c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - prints 0-9 using their ascii values
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
