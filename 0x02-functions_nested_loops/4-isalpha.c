572133bac96cd2c82fe3eb124b0bfea5ede0f061
Hope-Parnell <parnell.hope.c@gmail.com>
2021-06-28 15:47:12 +0000

    add function file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isalpha - checks if input is a letter
+ * @c: ascii value of a character
+ *
+ * Return: 1 if lowercase, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	int i;
+
+	if ((c <= 122 && c >= 97) || (c >= 65 && c <= 90))
+		i = 1;
+	else
+		i = 0;
+	return (i);
+}
