6eafbcf42b9d704f5d573076418f5e670bd1374e
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-01 07:34:38 +0000

    add function file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _isupper - checks if input character is a digit
+ * @c: the ascii value of the inputted character
+ *
+ * Return: 1 if digit, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if(c >= '0' && c <= '9')
+		return (1);
+	else
+		return(0);
+}
