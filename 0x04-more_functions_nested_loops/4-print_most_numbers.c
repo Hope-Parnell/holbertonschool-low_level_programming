53d93b244ccf86f10eb09e25dc0fd8579c4997cb
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-01 07:52:08 +0000

    add function file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - prints 1-9 except for 2 and 4 followed by a new line
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
