00910b60307f880781f7608e0d2beccda9935c63
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-01 08:19:14 +0000

    add function file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * print_diagonal - prints a diagonal line of specified length
+ * @n: length of the line
+ *
+ * Return:
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j < i ; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
