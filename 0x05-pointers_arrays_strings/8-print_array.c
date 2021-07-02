a45194c21d9649f02731b66dc64ffb0ebccd5e5e
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-02 20:38:52 +0000

    add function file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n items from an array
+ * @a: pointer to an array
+ * @n: number of items to be printed
+ *
+ * Return:
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n ; i++)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
+}
