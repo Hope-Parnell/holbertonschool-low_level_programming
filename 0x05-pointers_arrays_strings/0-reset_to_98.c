0efc8a02993a4cc5719d9619967ddf6157aa9c6f
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-02 17:49:41 +0000

    add function file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * reset_to_98 -takes a pointer and updates the value it points to to 98
+ * @n: a pontert to an integer
+ *
+ * Return:
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
