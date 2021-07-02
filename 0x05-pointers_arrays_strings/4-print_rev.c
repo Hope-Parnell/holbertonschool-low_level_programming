b2878d9b4ee5056316c13553920001b6b7fae70a
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-02 19:04:42 +0000

    add function file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string backwards
+ * @s: a string of characters
+ *
+ * Return:
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	for (i--; i => 0 ; i--)
+	{
+		putchar(s[i]);
+	}
+	_putchar('\n');
+}
