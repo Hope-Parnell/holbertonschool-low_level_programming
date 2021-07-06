1e1fb0b8ba65236418d832fef000ee5366ebde1b
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-06 16:49:06 +0000

    make Betty happy

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@
 int _atoi(char *s)
 {
 	int i, l, n, sign;
-   
+
 	n = 0;
 	sign = 1;
 	i = 0;
@@ -22,10 +22,18 @@ int _atoi(char *s)
 	for (i = 0; i < l; i++)
 	{
 		if (s[i] == '-' && (s[i + 1] >= '0' && s[i + 1] <= '9'))
-			sign = -1;
-		else if (s[i] >= '0' && s[i] <= '9')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			if ((n * -10) - (s[i] - 48) == -2147483648)
+			{
+				n = (n * -10) - (s[i] - 48);
+				return (n);
+			}
 			n = n * 10 + (s[i] - '0');
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
 	}
-	n = n * sign;
-	return (n);
+	return (n * sign);
 }
