8a0a79e1c97e87512d374b44137f2a230238407c
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-08 16:24:44 +0000

    add function file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strncat - combines strings
+ * @dest: pointer to first string
+ * @src: pointer to second string
+ * @n: number of characters from src to cat
+ *
+ * Return: pointer to the start of dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	char *ret = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+	return (ret);
+}
