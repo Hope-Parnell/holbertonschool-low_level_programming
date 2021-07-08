2d55945b8604f1eabd557246cdf380f39f6dfaec
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-08 17:29:25 +0000

    add function file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncpy - copies a n characters of a string
+ * @dest: pointer to destination string
+ * @src: pointer to original string
+ * @n: number of characters to copy
+ *
+ * Return: pointer to start of dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char* ret = dest;
+
+	while ((*src != '\0') && (n > 0))
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = *src;
+
+	return (ret);
+}
