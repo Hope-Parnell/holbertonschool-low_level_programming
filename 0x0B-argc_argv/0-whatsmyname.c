0e91eff4e2e26db6c8fefdb0e76d352ef049e9eb
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-15 18:43:39 +0000

    c program to print its name

---
+++ 0x0B-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the funtion
+ * @argc: argument counter
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
