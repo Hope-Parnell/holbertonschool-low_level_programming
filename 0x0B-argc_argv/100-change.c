da80be9b52fa1ad14aae203b4506f2ee3348b96e
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-15 19:55:11 +0000

    c code to make change

---
+++ 0x0B-argc_argv/100-change.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the min number of coins to make change
+ * @argc: argument counter
+ * @argv: array of arguments
+ *
+ * Return: 0 if exactly 1 argument is passed, 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int change, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	change = atoi(argv[1]);
+	while (change > 0)
+	{
+		while (change / 25 > 0)
+		{
+			change -= 25;
+			coins++;
+		}
+		while (change / 10 > 0)
+		{
+			change -= 10;
+			coins++;
+		}
+		while (change / 2 > 0)
+		{
+			change -= 2;
+			coins++;
+		}
+		change--;
+		coins++;
+	}
+	printf("%d\n", coins);
+
+	return (0);
+}
