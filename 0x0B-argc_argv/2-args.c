5e052695f66e44c37e2c65ebaca97f07f09b579f
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-15 19:01:01 +0000

    c code to print all arguments

---
+++ 0x0B-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all arguments passed to it
+ * @argc: argument counter
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
