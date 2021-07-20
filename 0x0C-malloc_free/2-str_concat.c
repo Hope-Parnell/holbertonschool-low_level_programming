63caaa555031a852128dce796cdeaed2c2198052
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-20 07:46:59 +0000

    changed nullbyte to empty string

--- 0x0C-malloc_free/2-str_concat.c
+++ 0x0C-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		*s1 = 0;
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = 0;
+		s2 = "";
 	for (size1 = 0; s1[size1] != 0; size1++)
 	{}
 	for (size2 = 0; s2[size2] != 0; size2++)
