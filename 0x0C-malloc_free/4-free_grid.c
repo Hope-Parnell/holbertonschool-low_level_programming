aca9437f9afefee1dbaab7a4490653ced10ea1ac
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-20 08:44:18 +0000

    add function file

---
+++ 0x0C-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees malloc from 2D array
+ * @grid: double pointer to 2d array
+ * @height: number of rows in array
+ *
+ * Return:
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i, j;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
