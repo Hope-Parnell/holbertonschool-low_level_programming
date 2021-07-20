ff106c2172a2788398338f3612d073a9fd2abc4a
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-20 06:51:05 +0000

    corrected output

--- 0x0C-malloc_free/0-create_array.c
+++ 0x0C-malloc_free/0-create_array.c
@@ -9,6 +9,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	int i;
 	char *ptr;
 
 	if (size == 0)
@@ -16,7 +17,10 @@ char *create_array(unsigned int size, char c)
 	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (NULL);
-	ptr[0] = c;
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
 
 	return (ptr);
 }
