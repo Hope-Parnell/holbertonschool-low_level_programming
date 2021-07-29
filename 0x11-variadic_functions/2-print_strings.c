0efce98f94f2b4da97ea50407fd4f2de700619c2
Hope-Parnell <parnell.hope.c@gmail.com>
2021-07-29 17:55:45 +0000

    make Betty happy

--- 0x11-variadic_functions/2-print_strings.c
+++ 0x11-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (s == NULL)
 			s = "(nil)";
 		printf("%s", s);
-		if (separator != NULL && i < (n - 1)) 
+		if (separator != NULL && i < (n - 1))
 			printf("%s", separator);
 	}
 	printf("\n");
