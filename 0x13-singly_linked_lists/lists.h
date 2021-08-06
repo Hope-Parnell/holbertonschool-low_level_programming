0e1b10ef80b97846d78b873b633f58cb1d929303
Hope-Parnell <parnell.hope.c@gmail.com>
2021-08-06 17:57:34 +0000

    make Betty happy

--- 0x13-singly_linked_lists/lists.h
+++ 0x13-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,9 +16,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
