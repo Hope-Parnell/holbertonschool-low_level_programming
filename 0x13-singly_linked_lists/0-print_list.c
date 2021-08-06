7bcc0e1c542343a28402fe3809f9f8a758f30e07
Hope-Parnell <parnell.hope.c@gmail.com>
2021-08-06 16:57:52 +0000

    add function to print singly linked lists

---
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a singly linked list
+ * @h - pointer to start of list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+	list_t *ptr = (list_t *)h;
+
+	if (!h)
+		return (0);
+	
+	while (ptr)
+	{
+		if (!ptr->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", ptr->len, ptr->str);
+		nodes++;
+		ptr = ptr->next;
+	}
+	return (nodes);
+}
