f2edc21e7fab6b550e776ae571a1f33587bd58ad
Hope-Parnell <parnell.hope.c@gmail.com>
2021-08-10 17:57:50 +0000

    add function file

---
+++ 0x14-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - finds a node in a singly linked list
+ * 
+ * @head: singly linked list
+ * @index: index of the node to find 
+ * Return: pointer to node, NULL if the node does not exist 
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *seek = head;
+	unsigned int i;
+
+	if (!head)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		if (!seek)
+			return (NULL);
+		seek = seek->next;
+	}
+
+	return (seek);
+}
