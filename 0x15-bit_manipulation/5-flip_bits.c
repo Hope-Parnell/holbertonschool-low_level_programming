5298eda655e332cdd9d6c7af1e612c22bb0a367a
Hope-Parnell <parnell.hope.c@gmail.com>
2021-08-12 17:13:31 +0000

    add function file

---
+++ 0x15-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - checks how many bits differ between 2 numbers
+ * @n: first number
+ * @m: second number
+ * Return: number of differing bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, flip = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if ((n >> i) & 1 != (m >> i) & 1)
+			flip++;
+	}
+	return (flip);
+}
