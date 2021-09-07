35df915710687c37c1861dc6cb76531a2ae82877
Hope-Parnell <parnell.hope.c@gmail.com>
2021-09-07 14:29:00 +0000

    add function file

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the contents of a doubly linked list
+ * @h: pointer to a node in the list
+ *
+ * Return: number of nodes in the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	dlistint_t *seek, *head;
+	size_t nodes;
+
+	if (!h)/*check if list is NULL*/
+		return (0);
+	head = (dlistint_t *)h;
+	while (head->prev) /*find the head*/
+		head = head->prev;
+	seek = head;
+	for (nodes = 0; seek != NULL; nodes++)/*print each integer*/
+	{
+		printf("%d\n", seek->n);
+		seek = seek->next;
+	}
+	return (nodes);
+}
