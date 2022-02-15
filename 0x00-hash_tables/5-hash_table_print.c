9ed8c4a49dbcee4ed4d66508f885d45360db0eb1
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-15 11:12:22 -0600

    task 5

--- 0x00-hash_tables/5-hash_table_print.c
+++ 0x00-hash_tables/5-hash_table_print.c
@@ -1,7 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always EXIT_SUCCESS.
+ * hash_table_print - prints a hash table
+ * @ht: pointer to the hash table
  */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, comma = 0;
+	hash_node_t *current;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		for (current = ht->array[i]; current; current = current->next)
+		{
+			if (comma == 1)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			comma = 1;
+		}
+	}
+	printf("}\n");
+}
