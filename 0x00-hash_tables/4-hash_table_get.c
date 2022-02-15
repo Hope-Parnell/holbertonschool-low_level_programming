21d800b426c751af15e8476f0a616a73e96e7a19
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-15 10:39:23 -0600

    task 4

--- 0x00-hash_tables/4-hash_table_get.c
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -1,7 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
+ * hash_table_get - retrieves a value associated with a key
+ * @ht:
+ * @key:
  *
- * Return: Always EXIT_SUCCESS.
+ * Return: the value or null
  */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *parser;
+
+	if (!ht || !key || strcmp(key, "") == 0)
+		return (NULL);
+	i = key_index((const unsigned char *)key, ht->size);
+	for (parser = ht->array[i]; parser; parser = parser->next)
+	{
+		if (strcmp(key, parser->key) == 0)
+		{
+			return (parser->value);
+		}
+	}
+	return (NULL);
+}
