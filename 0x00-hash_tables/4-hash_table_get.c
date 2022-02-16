53ee6c173409db269bb2dc7229308d09fd1e54c9
Hope-Parnell <parnell.hope.c@gmail.com>
2022-02-16 09:32:27 -0600

    completed Betty documentation

--- 0x00-hash_tables/4-hash_table_get.c
+++ 0x00-hash_tables/4-hash_table_get.c
@@ -2,10 +2,10 @@
 
 /**
  * hash_table_get - retrieves a value associated with a key
- * @ht:
- * @key:
+ * @ht: the hash table
+ * @key: which key to find
  *
- * Return: the value or null
+ * Return: the value or null if not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
